a87acb5e4f8aa6448a21c3980cabe9d71d1d60fc
Space Cowboy <dxb@boardmansgame.com>
2026-01-14 22:50:05 +0000

    Update changes to work on dev server

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,6 @@ export default defineConfig({
     },
   },
   server: {
-    allowedHosts: ['.tryhydrogen.dev'],
+    allowedHosts: ['traceminerals.boardmansgame.com', '.tryhydrogen.dev'],
   },
 });
